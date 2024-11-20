434d34f614de643cf44ebca70464ba61ce98dbc6
vejaxu <wjxu29@gmail.com>
2024-11-20 21:47:49 +0800

    update for reviewing

--- vit_pytorch/vit.py
+++ vit_pytorch/vit.py
@@ -25,6 +25,20 @@ class FeedForward(nn.Module):
 
     def forward(self, x):
         return self.net(x)
+    
+class ffn(nn.Module):
+    def __init__(self, dim, hidden_size, dropout) -> None:
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim, hidden_size), 
+            nn.GELU(), 
+            nn.Dropout(dropout), 
+            nn.Linear(hidden_size, dim), 
+            nn.Dropout(dropout)
+        )
+    def forward(self, x):
+        return self.net(x)
 
 class Attention(nn.Module):
     def __init__(self, dim, heads = 8, dim_head = 64, dropout = 0.):
